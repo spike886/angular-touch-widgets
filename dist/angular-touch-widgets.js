09a185962d867bbc622982c43c61bb8942445c8f
Mohamed Habshey <Mohamed.habshey10@gmail.com>
2016-10-11 11:47:49 +0200

    commit the distrbution file to the repo

--- dist/angular-touch-widgets.js
+++ dist/angular-touch-widgets.js
@@ -275,7 +275,48 @@ function parseTime(time) {
 }
 
 "use strict";
-angular.module('angularTouchWidgets.directives', ['angularTouchWidgets.directives.modeSelector', 'angularTouchWidgets.directives.lightViewer', 'angularTouchWidgets.directives.lightColorEditor', 'angularTouchWidgets.directives.lightIntensityEditor', 'angularTouchWidgets.directives.clockViewer', 'angularTouchWidgets.directives.timerViewer', 'angularTouchWidgets.directives.clockEditor', 'angularTouchWidgets.directives.timerEditor', 'angularTouchWidgets.directives.thermometerEditor', 'angularTouchWidgets.directives.phViewer', 'angularTouchWidgets.directives.orpViewer', 'angularTouchWidgets.directives.onOffButton']);
+angular.module('angularTouchWidgets.directives.colorWheel', []).directive('colorWheel', function($animate) {
+  return {
+    restrict: "E",
+    replace: true,
+    scope: {
+      color: '=',
+      img: '@'
+    },
+    template: '<div style="height: 300px; width: 300px; margin: auto; position: relative;">\
+                    <canvas var="1" width="300" height="300" ng-click="colorClick($event)"></canvas>\
+                    <div ng-style="{\'background-color\': \'rgb(\'+color.r+\',\'+color.g+\',\'+color.b+\')\'}" style="position: absolute; top: 120px; left: 120px; height: 60px; width: 60px; border-radius: 50%; box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.46); transition: background 0.1s; -webkit-transition: background 0.1s"></div>\
+                </div>',
+    link: function(scope, element) {
+      var ctx = element.children()[0].getContext('2d');
+      var selection = angular.element(element.children()[1]);
+      var image = new Image();
+      image.onload = function() {
+        ctx.drawImage(image, 0, 0, 300, 300);
+      };
+      image.src = scope.img || 'img/color-wheel.png';
+      var animateClick = function() {
+        $animate.addClass(selection, 'pulse animated-quick').then(function() {
+          selection.removeClass('pulse animated-quick');
+        });
+      };
+      scope.colorClick = function(e) {
+        var pixel = ctx.getImageData(e.offsetX, e.offsetY, 1, 1).data;
+        if (pixel[3] !== 0) {
+          angular.copy({
+            r: pixel[0],
+            g: pixel[1],
+            b: pixel[2]
+          }, scope.color);
+          animateClick();
+        }
+      };
+    }
+  };
+});
+
+"use strict";
+angular.module('angularTouchWidgets.directives', ['angularTouchWidgets.directives.modeSelector', 'angularTouchWidgets.directives.colorWheel', 'angularTouchWidgets.directives.lightViewer', 'angularTouchWidgets.directives.lightColorEditor', 'angularTouchWidgets.directives.lightIntensityEditor', 'angularTouchWidgets.directives.clockViewer', 'angularTouchWidgets.directives.timerViewer', 'angularTouchWidgets.directives.clockEditor', 'angularTouchWidgets.directives.timerEditor', 'angularTouchWidgets.directives.thermometerEditor', 'angularTouchWidgets.directives.phViewer', 'angularTouchWidgets.directives.orpViewer', 'angularTouchWidgets.directives.onOffButton']);
 function regularArcData(cx, cy, radius, startDegrees, endDegrees, isCounterClockwise) {
   var offsetRadians = 0;
   var sweepFlag = (isCounterClockwise) ? 0 : 1;
